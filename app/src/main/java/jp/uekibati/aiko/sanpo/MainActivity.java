dfa97473322f5dcc8368ab421dbcc9b2915be90d
yuiseki <yuiseki@gmail.com>
2019-07-29 22:11:53 +0900

    コピペしてきたやつ

--- app/src/main/java/jp/uekibati/aiko/sanpo/MainActivity.java
+++ app/src/main/java/jp/uekibati/aiko/sanpo/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+
 public class MainActivity extends AppCompatActivity {
+    public static int REQUEST_OAUTH_REQUEST_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .build();
+
+        if (!GoogleSignIn.hasPermissions(
+                GoogleSignIn.getLastSignedInAccount(this),
+                fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions
+            );
+        } else {
+            readData();
+        }
+    }
+
+    private void readData() {
+
     }
 }
