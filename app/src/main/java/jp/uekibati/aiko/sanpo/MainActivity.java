cea214edbfc1358984d74503e2b6553529ab781f
yuiseki <yuiseki@gmail.com>
2019-08-10 20:19:01 +0900

    Googleアカウントでログインできるようにする

--- app/src/main/java/jp/uekibati/aiko/sanpo/MainActivity.java
+++ app/src/main/java/jp/uekibati/aiko/sanpo/MainActivity.java
@@ -30,7 +30,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
@@ -44,15 +48,21 @@ import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static String TAG = "MainActivity";
+    private static int RC_SIGN_IN = 1000;
     private SensorManager sensorManager;
     private TextView logTextView; // ログ表示
     private Integer sumOfStep = 0;
-
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,13 +74,71 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
 
-        // ログを表示するTextViewの初期化
+        // ログを表示するTextViewを初期化する
         logTextView = (TextView)findViewById(R.id.text_results);
+
+        // 認証を初期化する
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // DBを初期化する
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    }
+
+    public void updateUI(GoogleSignInAccount account){
+        if (account==null){
+            Log.d(TAG, "login failed...");
+        }else{
+            Log.d(TAG, "login success: "+account.getEmail());
+            View view = this.findViewById(android.R.id.content);
+            if (view == null) return;
+            Snackbar.make(view, account.getEmail()+"でログインしています", Snackbar.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account==null){
+            updateUI(account);
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }else {
+            updateUI(account);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                // Signed in successfully, show authenticated UI.
+                updateUI(account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                updateUI(null);
+            }
+        }
     }
 
     @Override
